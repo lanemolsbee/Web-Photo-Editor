f5904139e545793e0bd9f95fde2a66f4dc8bc48d
Lane Molsbee <lanemolsbee@gmail.com>
2025-05-30 15:29:02 -0700

    Installed mongodb and express.js, and also wrote the code to serve the index.html page

--- server.js
+++ server.js
@@ -2,4 +2,15 @@ var express = require('express')
 var app = express()
 var path = require('path')
 var {MongoClient} = require('mongodb')
+const PORT = 8080;
 const client = new MongoClient('mongodb://localhost:27017')
+
+app.use('/Stylings', express.static(path.join(__dirname, 'Stylings')));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'HTML Files', 'index.html'))
+});
+
+app.listen(8080, () =>{
+	console.log(`Server is running at http://localhost:${PORT}`);
+})
